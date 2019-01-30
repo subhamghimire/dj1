cef9290d29bf347dcb8637fd0a750adf3d5a463e
subham <subham.ghimire26@gmail.com>
2019-01-30 18:44:50 +0545

    fourth commit

--- dj1/urls.py
+++ dj1/urls.py
@@ -2,6 +2,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('',views.homepage),
+path('',views.homepage,name='home'),
 path('countss/',views.count,name='count')
 ]
--- dj1/views.py
+++ dj1/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'index.html' , {'name':'this is subham ghimire','address':'hetaudachildrenpark'})
@@ -18,5 +19,5 @@ def count(request):
             dictword[word] = 1
 
 
-
-    return render(request, 'count.html',{'text':data,'words':length ,'worddictionary':dictword})
+    sorted_list = sorted(dictword.items(),key = operator.itemgetter(1), reverse = True)
+    return render(request, 'count.html',{'text':data,'words':length ,'worddictionary':sorted_list})
