21ed2f8dc3d698f6eaa90c026b689ac6374072a7
Shekhar ghimire <subham.ghimire26@gmail.com>
2019-02-04 19:24:01 +0545

    debug set to false

--- dj1/settings.py
+++ dj1/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's*gbl1)tfa38t32%w)5a&q%ln_*htbv94fxxfr_f)6ms4q!-bb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
